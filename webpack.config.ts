be72c7fb80f4fe752e6bad13cc42a02a5529e0c3
Vladimir Kochenov <kochenov.vl@gmail.com>
2024-01-20 15:23:57 +0300

    Add webpack-dev-server and source map for development mode

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,13 +2,18 @@ import path from 'path'
 import webpack from 'webpack'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 
+import type { Configuration as DevServerConfiguration } from 'webpack-dev-server'
+
 type Mode = 'production' | 'development'
 
 interface EnvVariables {
   mode: Mode
+  port: number
 }
 
 export default (env: EnvVariables) => {
+  const isDev = env.mode === 'development'
+
   const config: webpack.Configuration = {
     mode: env.mode ?? 'development',
     entry: path.resolve(__dirname, 'src', 'index.ts'),
@@ -19,8 +24,8 @@ export default (env: EnvVariables) => {
     },
     plugins: [
       new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
-      new webpack.ProgressPlugin()
-    ],
+      isDev && new webpack.ProgressPlugin()
+    ].filter(Boolean),
     module: {
       rules: [
         {
@@ -32,7 +37,14 @@ export default (env: EnvVariables) => {
     },
     resolve: {
       extensions: ['.tsx', '.ts', '.js']
-    }
+    },
+    devtool: isDev && 'inline-source-map',
+    devServer: isDev
+      ? {
+          port: env.port ?? 4242,
+          open: false
+        }
+      : undefined
   }
 
   return config
