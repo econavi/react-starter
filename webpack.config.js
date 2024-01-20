d9ad6b17d4f2f0934f6cadd775bee393402c1e01
Vladimir Kochenov <kochenov.vl@gmail.com>
2024-01-20 14:04:14 +0300

    Add typescript loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = (env) => {
   return {
     mode: env.mode ?? 'development',
-    entry: path.resolve(__dirname, 'src', 'index.js'),
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
     output: {
       path: path.resolve(__dirname, 'build'),
       filename: '[name].[contenthash:8].js',
@@ -14,6 +14,18 @@ module.exports = (env) => {
     plugins: [
       new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
       new webpack.ProgressPlugin()
-    ]
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js']
+    }
   }
 }
