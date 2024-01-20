e78ca06b15a47a3eb6a3750bd38140cbf83ef08a
Vladimir Kochenov <kochenov.vl@gmail.com>
2024-01-20 17:25:38 +0300

    Support css and install styled-components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,13 @@
+import styled from 'styled-components'
+
 export const App = () => {
   return (
     <div>
-      <h1>Hello world</h1>
+      <Title>Hello world</Title>
     </div>
   )
 }
+
+const Title = styled.h1`
+  color: white;
+`
