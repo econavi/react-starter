cc5326eb5c2e7eff71c9866db19d506ee5870f0a
Vladimir Kochenov <kochenov.vl@gmail.com>
2024-01-22 15:27:09 +0300

    Add support absolute imports

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,7 +1,11 @@
 import { Configuration } from 'webpack'
+import { BuildOptions } from './types/types'
 
-export const buildResolvers = (): Configuration['resolve'] => {
+export const buildResolvers = (options: BuildOptions): Configuration['resolve'] => {
   return {
-    extensions: ['.tsx', '.ts', '.js']
+    extensions: ['.tsx', '.ts', '.js'],
+    preferAbsolute: true,
+    modules: [options.paths.src, 'node_modules'],
+    alias: {}
   }
 }
--- config/build/buildWebpack.ts
+++ config/build/buildWebpack.ts
@@ -21,7 +21,7 @@ export const buildWebpack = (options: BuildOptions): webpack.Configuration => {
       rules: buildLoaders()
     },
     plugins: buildPlugins(options),
-    resolve: buildResolvers(),
+    resolve: buildResolvers(options),
     devtool: isDev && 'inline-source-map',
     devServer: isDev ? buildDevServer(options) : undefined
   }
