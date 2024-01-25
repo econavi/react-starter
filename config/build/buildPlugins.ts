8c2f6082c43b0f46ae9a381aa29b060067e5d5c3
Vladimir Kochenov <kochenov.vl@gmail.com>
2024-01-25 10:48:58 +0300

    Add robots.txt and install copy-webpack-plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import webpack, { DefinePlugin, type Configuration } from 'webpack'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
+import CopyPlugin from 'copy-webpack-plugin'
 
 import type { BuildOptions } from './types/types'
 import path from 'path'
@@ -10,6 +11,7 @@ type Plugins = Configuration['plugins']
 
 export const buildPlugins = (options: BuildOptions): Plugins => {
   const isDev = options.mode === 'development'
+  const isProd = options.mode === 'production'
 
   const plugins: Plugins = [
     new HtmlWebpackPlugin({
@@ -25,6 +27,14 @@ export const buildPlugins = (options: BuildOptions): Plugins => {
     plugins.push(new webpack.ProgressPlugin())
     plugins.push(new ForkTsCheckerWebpackPlugin({ devServer: false }))
     plugins.push(new ReactRefreshWebpackPlugin())
+  }
+
+  if (isProd) {
+    plugins.push(
+      new CopyPlugin({
+        patterns: [{ from: path.resolve(options.paths.public, 'robots.txt'), to: options.paths.output }]
+      })
+    )
   }
 
   return plugins
